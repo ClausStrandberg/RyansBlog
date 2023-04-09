486939de44c34045793128f45b821db771d73658
hakanalmer <hakanalmer@outlook.com>
2023-04-09 16:35:14 +0800

    更新部分样式,更新名称

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
-export const SITE_TITLE = `Air Blog`;
+export const SITE_TITLE = `Pog的窝点`;
 export const SITE_DESCRIPTION = 'Site Description';
 export const SITE_EMAIL = 'no.sql@qq.com'
 export const SITE_NAME = 'astro.yufengbiji.com';
