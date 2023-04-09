db2bbbd64ecf0e25cc317f80a101366260f3e40f
hakanalmer <hakanalmer@outlook.com>
2023-04-09 16:37:45 +0800

    更新名称

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Pog的窝点`;
-export const SITE_DESCRIPTION = 'Site Description';
+export const SITE_TITLE = `Ryan Brisko's Blog`;
+export const SITE_DESCRIPTION = '这里是Ryan的blog';
 export const SITE_EMAIL = 'no.sql@qq.com'
 export const SITE_NAME = 'astro.yufengbiji.com';
 export const SITE_URL = "https://astro.yufengbiji.com";
