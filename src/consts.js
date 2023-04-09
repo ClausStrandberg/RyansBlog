3637c84615e0c4de1173b459206d9f4044f49c51
Ryan Brisko <120040376+RyanBrisko@users.noreply.github.com>
2023-04-09 16:51:09 +0800

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Ryan Brisko's Blog`;
+export const SITE_TITLE = `Ryan的窝点`;
 export const SITE_DESCRIPTION = '这里是Ryan的blog';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_EMAIL = 'RyanBrisko@gmail.com'
+export const SITE_NAME = 'Ryan‘s Log Station';
+export const SITE_URL = "https://ryans-blog-ebon.vercel.app/";
